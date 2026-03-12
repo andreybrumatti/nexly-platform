157c252689d90e012152513867d1597c58b6c319
andreybrumatti <andreyporto004@gmail.com>
2026-03-12 06:27:34 -0300

    first commit

---
+++ resources/js/components/app-logo-image.tsx
@@ -0,0 +1,9 @@
+export default function AppLogoImage({...props}) {
+        return (
+                <img
+                    src="/assets/simbol-logo.jpg"
+                    alt="Nexly Plataform" {...props}
+                    className={"h-8 w-40 p-1 object-fill"}
+                 />
+    );
+}
---
+++ resources/js/components/app-logo.tsx
@@ -0,0 +1,19 @@
+import AppLogoImage from '@/components/app-logo-image';
+
+export default function AppLogo() {
+    return (
+        <>
+            <div className="flex aspect-square size-10 items-center justify-center rounded-md bg-sidebar-primary text-sidebar-primary-foreground">
+                <AppLogoImage className="size-full fill-current text-white dark:text-black" />
+            </div>
+            <div className="ml-1 grid flex-1 text-left text-sm">
+                <span className="mb-0.5 truncate leading-tight font-semibold">
+                    Nexly
+                </span>
+                <span className="text-muted-foreground">
+                    Plataform
+                </span>
+            </div>
+        </>
+    );
+}
